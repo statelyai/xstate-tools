8c3d1929364d82cb340786bd82683d3c40f89a97
Matt <mpocock@me.com>
2021-07-14 14:41:53 +0100

    WIP

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -3,68 +3,72 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import * as path from "path";
+import { workspace, ExtensionContext } from "vscode";
 
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind,
-} from 'vscode-languageclient/node';
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+  // The server is implemented in node
+  let serverModule = context.asAbsolutePath(
+    path.resolve(__dirname, "../", "server", "out", "server.js"),
+  );
+  // The debug options for the server
+  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+  let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions,
-		},
-	};
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  let serverOptions: ServerOptions = {
+    run: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+    },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
 
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [
-			{ scheme: 'file', language: 'typescript' },
-			{
-				scheme: 'file',
-				language: 'javascript',
-			},
-			{ scheme: 'file', language: 'typescriptreact' },
-			{
-				scheme: 'file',
-				language: 'javascriptreact',
-			},
-		],
-	};
+  // Options to control the language client
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      { scheme: "file", language: "typescript" },
+      {
+        scheme: "file",
+        language: "javascript",
+      },
+      { scheme: "file", language: "typescriptreact" },
+      {
+        scheme: "file",
+        language: "javascriptreact",
+      },
+    ],
+  };
 
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'xstateLanguageServer',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
+  // Create the language client and start the client.
+  client = new LanguageClient(
+    "xstateLanguageServer",
+    "XState (Demo)",
+    serverOptions,
+    clientOptions,
+  );
 
-	// Start the client. This will also launch the server
-	client.start();
+  // Start the client. This will also launch the server
+  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
