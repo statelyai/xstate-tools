03869b5fc51491a3e492a52670af00491873fe3f
Matt Pocock <mattpocockvoice@gmail.com>
2022-01-25 14:30:53 +0000

    Fixed constants

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,3 @@
-// TODO - fix login issues
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = "https://stately.ai";
 export const TOKEN_KEY = `stately-editor-key#${BASE_URL}`;
 export const EXTENSION_ID = "mattpocock.xstate-vscode";
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
-import type { Node } from "@babel/types";
 import { TextDocument, TextEdit } from "vscode-languageserver-textdocument";
 import {
   CompletionItem,
