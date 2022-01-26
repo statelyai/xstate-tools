375f8ab624489021238ae9fa94f672b88f22a149
Matt Pocock <mattpocockvoice@gmail.com>
2022-01-26 10:05:04 +0000

    Fixed extension id

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://stately.ai";
 export const TOKEN_KEY = `stately-editor-key#${BASE_URL}`;
-export const EXTENSION_ID = "mattpocock.xstate-vscode";
+export const EXTENSION_ID = "statelyai.stately-vscode";
