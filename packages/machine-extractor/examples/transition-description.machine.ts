cd41ed43835a604a5283836c4b24ffd667f17621
jonathane1 <jonathane@sea.com>
2022-04-01 17:10:25 +0800

    adjust to spaces

--- packages/machine-extractor/examples/transition-description.machine.ts
+++ packages/machine-extractor/examples/transition-description.machine.ts
@@ -1,30 +1,30 @@
 import { createMachine } from "xstate";
 
 export const transitionDescription = createMachine({
-	id: "TransitionDescriptionMachine",
-	type: "parallel",
-	states: {
-		Ticket: {
-			initial: "Open",
-			states: {
-				Open: {
-					on: {
-						cancel: {
-							actions: "cancel ticket",
-							target: "Canceled",
-							description: "This is description example",
-						},
-						secondCancel: {
+  id: "TransitionDescriptionMachine",
+  type: "parallel",
+  states: {
+    Ticket: {
+      initial: "Open",
+      states: {
+        Open: {
+          on: {
+            cancel: {
+              actions: "cancel ticket",
+              target: "Canceled",
+              description: "This is description example",
+            },
+            secondCancel: {
               actions: "Secondary cancel ticket",
               description: 
-								`This is the
-								second example`,
+                `This is the
+                second example`,
               target: "Canceled",
             },
-					},
-				},
-				Canceled: {},
-			},
-		},
-	},
+          },
+        },
+        Canceled: {},
+      },
+    },
+  },
 });
