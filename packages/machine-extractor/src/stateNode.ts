cb1182beaae7549b37ff7b9e2039739a636a6bb1
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-06-09 17:08:46 +0200

    Add `predictableActionArguments` extractor and tweak types

--- packages/machine-extractor/src/stateNode.ts
+++ packages/machine-extractor/src/stateNode.ts
@@ -1,21 +1,14 @@
-import { types as t } from "@babel/core";
 import { MaybeArrayOfActions } from "./actions";
 import { Context } from "./context";
 import { History } from "./history";
 import { Invoke } from "./invoke";
 import { StateMeta } from "./meta";
-import {
-  AnyNode,
-  BooleanLiteral,
-  NumericLiteral,
-  StringLiteral,
-} from "./scalars";
+import { AnyNode, BooleanLiteral, StringLiteral } from "./scalars";
 import { Schema } from "./schema";
 import { MaybeTransitionArray } from "./transitions";
 import { TsTypes } from "./tsTypes";
 import { AnyParser } from "./types";
 import {
-  GetObjectKeysResult,
   GetParserResult,
   maybeArrayOf,
   objectOf,
@@ -42,7 +35,6 @@ export type StateNodeReturn = {
   id?: GetParserResult<typeof StringLiteral>;
   initial?: GetParserResult<typeof StringLiteral>;
   type?: GetParserResult<typeof StringLiteral>;
-  delimiter?: GetParserResult<typeof StringLiteral>;
   entry?: GetParserResult<typeof MaybeArrayOfActions>;
   exit?: GetParserResult<typeof MaybeArrayOfActions>;
   onEntry?: GetParserResult<typeof MaybeArrayOfActions>;
@@ -55,37 +47,30 @@ export type StateNodeReturn = {
   history?: GetParserResult<typeof History>;
   tags?: GetParserResult<typeof Tags>;
   states?: GetParserResult<AnyParser<ObjectOfReturn<StateNodeReturn>>>;
-  node: t.Node;
   meta?: GetParserResult<typeof StateMeta>;
-  tsTypes?: GetParserResult<typeof TsTypes>;
-  context?: GetParserResult<typeof Context>;
   data?: GetParserResult<typeof AnyNode>;
-  schema?: GetParserResult<typeof Schema>;
-  preserveActionOrder?: GetParserResult<typeof BooleanLiteral>;
   parallel?: GetParserResult<typeof BooleanLiteral>;
   description?: GetParserResult<typeof StringLiteral>;
-  key?: GetParserResult<typeof StringLiteral>;
-  version?: GetParserResult<typeof AnyNode>;
   activities?: GetParserResult<typeof AnyNode>;
+
+  // those should only be allowed at the root level
+  tsTypes?: GetParserResult<typeof TsTypes>;
+  schema?: GetParserResult<typeof Schema>;
+  context?: GetParserResult<typeof Context>;
+  preserveActionOrder?: GetParserResult<typeof BooleanLiteral>;
+  predictableActionArguments?: GetParserResult<typeof BooleanLiteral>;
   strict?: GetParserResult<typeof BooleanLiteral>;
-  // TODO
-  parent?: GetParserResult<typeof AnyNode>;
-  order?: GetParserResult<typeof NumericLiteral>;
-};
+  version?: GetParserResult<typeof AnyNode>;
+  delimiter?: GetParserResult<typeof StringLiteral>;
+  key?: GetParserResult<typeof StringLiteral>;
+} & ObjectPropertyInfo;
 
-type WithObjectPropertyInfo<T extends Record<any, any>> = {
-  [K in keyof T]: T[K] & ObjectPropertyInfo;
-};
-
-const StateNodeObject: AnyParser<WithObjectPropertyInfo<StateNodeReturn>> =
+const StateNodeObject: AnyParser<StateNodeReturn> =
   objectTypeWithKnownKeys(() => ({
     id: StringLiteral,
     initial: StringLiteral,
     type: StringLiteral,
-    tsTypes: TsTypes,
-    schema: Schema,
     history: History,
-    delimiter: StringLiteral,
     entry: MaybeArrayOfActions,
     exit: MaybeArrayOfActions,
     onEntry: MaybeArrayOfActions,
@@ -98,17 +83,21 @@ const StateNodeObject: AnyParser<WithObjectPropertyInfo<StateNodeReturn>> =
     tags: Tags,
     states: objectOf(StateNodeObject),
     meta: StateMeta,
-    context: Context,
     data: AnyNode,
-    preserveActionOrder: BooleanLiteral,
     parallel: BooleanLiteral,
     description: StringLiteral,
-    version: AnyNode,
-    key: StringLiteral,
     activities: AnyNode,
-    parent: AnyNode,
+
+    // those should only be allowed at the root level
+    tsTypes: TsTypes,
+    schema: Schema,
+    context: Context,
+    preserveActionOrder: BooleanLiteral,
+    predictableActionArguments: BooleanLiteral,
     strict: BooleanLiteral,
-    order: NumericLiteral,
+    version: AnyNode,
+    delimiter: StringLiteral,
+    key: StringLiteral,
   }));
 
 export const StateNode = StateNodeObject;
