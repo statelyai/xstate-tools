87b8f3eece33535f6655672139a3082a1aa79588
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-06-29 17:28:16 +0200

    Fixed weird file formatting

--- packages/shared/src/__tests__/__examples__/event-causing-delay.ts
+++ packages/shared/src/__tests__/__examples__/event-causing-delay.ts
@@ -1,31 +1,34 @@
 import { createMachine } from "xstate";
 
-createMachine({
-  tsTypes: {} as import("./event-causing-delay.typegen").Typegen0,
-  schema: {
-    events: {} as {
-      type: "NEXT";
-    },
-  },
-  initial: "a",
-  states: {
-    a: {
-      on: {
-        NEXT: "b",
+createMachine(
+  {
+    tsTypes: {} as import("./event-causing-delay.typegen").Typegen0,
+    schema: {
+      events: {} as {
+        type: "NEXT";
       },
     },
-    b: {
-      after: {
-        myDelay: "c",
+    initial: "a",
+    states: {
+      a: {
+        on: {
+          NEXT: "b",
+        },
+      },
+      b: {
+        after: {
+          myDelay: "c",
+        },
+      },
+      c: {},
+    },
+  },
+  {
+    delays: {
+      myDelay: (ctx, ev) => {
+        ((_accept: "NEXT") => {})(ev.type);
+        return 10;
       },
     },
-    c: {},
-  },
-}, {
-	delays: {
-		myDelay: (ctx, ev) => {
-			((_accept: 'NEXT') => {})(ev.type)
-			return 10
-		}
-	}
-});
+  }
+);
