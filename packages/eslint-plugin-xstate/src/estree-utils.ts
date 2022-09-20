2c7c003128f48b7dd1efb1f5b29ea703d6808e7f
with-heart <with.heart+git@pm.me>
2022-09-20 16:02:36 -0400

    add "no-emitted-from" rule

--- packages/eslint-plugin-xstate/src/estree-utils.ts
+++ packages/eslint-plugin-xstate/src/estree-utils.ts
@@ -5,6 +5,11 @@ const is = <Type extends AST_NODE_TYPES>(type: Type) =>
 
 export const isCallExpression = is(AST_NODE_TYPES.CallExpression);
 export const isIdentifier = is(AST_NODE_TYPES.Identifier);
+export const isImportNamespaceSpecifier = is(
+  AST_NODE_TYPES.ImportNamespaceSpecifier
+);
+export const isImportSpecifier = is(AST_NODE_TYPES.ImportSpecifier);
+export const isLiteral = is(AST_NODE_TYPES.Literal);
 export const isMemberExpression = is(AST_NODE_TYPES.MemberExpression);
 export const isObjectExpression = is(AST_NODE_TYPES.ObjectExpression);
 export const isProperty = is(AST_NODE_TYPES.Property);
