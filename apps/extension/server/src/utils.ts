8a7c15f95a83536743b0638066e87e21ab05a74f
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-12-09 20:43:40 +0100

    Remove `isErrorWithMessage`

--- apps/extension/server/src/utils.ts
+++ apps/extension/server/src/utils.ts
@@ -1,16 +1,6 @@
 import { MachineExtractResult } from '@xstate/machine-extractor';
 import { TypegenData } from '@xstate/tools-shared';
 
-export const isErrorWithMessage = (
-  error: unknown,
-): error is {
-  message: string;
-} =>
-  typeof error === 'object' &&
-  error !== null &&
-  'message' in error &&
-  typeof (error as Record<string, unknown>).message === 'string';
-
 export const getErrorMessage = (error: unknown): string => {
   if (
     !error ||
