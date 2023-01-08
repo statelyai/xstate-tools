d572bc5702f226558f7e69d7f0dad23536be2a70
David Khourshid <davidkpiano@gmail.com>
2023-01-08 08:48:56 -0500

    Add back layout string

--- apps/extension/client/testFixture/machine.ts
+++ apps/extension/client/testFixture/machine.ts
@@ -1,5 +1,6 @@
 import { createMachine } from 'xstate';
 
+/** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgAoBbAQwGMALASwzAEoA6KiAGzHxAActYqAXKlgwcAHogAsAJnQBPRAA4ArA3EBOdYtUBmAAyr5qxfPHI0IYuWq06HbrwFDRiAIzzZL+Qx3efv3wHZTUyA */
 createMachine(
   {
     id: '(machine)',
