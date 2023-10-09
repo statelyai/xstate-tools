27a8eceef6d089f6e22026174a4567a0b91fa85f
Anders Bech Mellson <mellson@icloud.com>
2023-10-09 10:14:25 +0200

    wip

--- apps/cli/src/__tests__/__sky__/actorFromStately.ts
+++ apps/cli/src/__tests__/__sky__/actorFromStately.ts
@@ -1,6 +1,6 @@
 import { actorFromStately } from '@statelyai/sky';
 
-// TODO: The Sky SDK is using xstate v5 so not everything is typed correctly
+// TODO: The Sky SDK is using xstate v5 so we can't really create meaningful test until we upgrade to v5 in this repo
 async function testActor() {
   const actor = await actorFromStately({
     url: 'https://sky.dev.stately.ai/th413d',
--- apps/cli/src/__tests__/__sky__/machineFromStately.ts
+++ apps/cli/src/__tests__/__sky__/machineFromStately.ts
@@ -1,11 +1,12 @@
 import { machineFromStately } from '@statelyai/sky';
-import { interpret } from 'xstate';
+import { createActor } from 'xstate-beta';
 import { skyConfig } from './machineFromStately.sky';
 
-// TODO: The Sky SDK is using xstate v5 so not everything is typed correctly
+// TODO: The Sky SDK is using xstate v5 so we can't really create meaningful test until we upgrade to v5 in this repo
 const machine = machineFromStately(
   { url: 'https://sky.dev.stately.ai/th413d' },
-  skyConfig,
+  skyConfig as any,
 );
-const actor = interpret(machine).start();
+
+const actor = createActor(machine as any).start();
 actor.send({ type: 'INSERT_COINS' });
