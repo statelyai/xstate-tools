bf6454601928d187f037751685700070a707d65e
Mateusz Burzyński <mateuszburzynski@gmail.com>
2023-12-21 17:48:59 +0100

    Initial code of the unrefined `ts-project` pkg

--- new-packages/language-server/src/serverPlugin.ts
+++ new-packages/language-server/src/serverPlugin.ts
@@ -1,4 +1,6 @@
 import { Connection, ServerPlugin } from '@volar/language-server';
+import { XStateProject, createProject } from '@xstate/ts-project';
+import type { Program } from 'typescript';
 import {
   Provide,
   create as createTypeScriptService,
@@ -27,38 +29,54 @@ export const createXStateServerPlugin =
         'tsx',
       ],
 
-      async resolveConfig(config, env, projectContext) {
+      async resolveConfig(config) {
         if (!modules.typescript) {
           throw new Error('TypeScript module is missing');
         }
 
-        (config.services ||= {}).typescript = createTypeScriptService(
+        (config.services ||= {}).typescript ||= createTypeScriptService(
           modules.typescript,
         );
 
         return config;
       },
       onInitialized(projects) {
+        const projectCache = new WeakMap<Program, XStateProject>();
+
         connection.onRequest(helloRequest, async ({ textDocument, name }) => {
           if (!textDocument) {
             connection.console.log(`Hello, ${name}!`);
             return true;
           }
-          const languageService = await getTypeScriptLanguageService(
-            textDocument.uri,
+          const tsProgram = (
+            await getTypeScriptLanguageService(textDocument.uri)
+          ).getProgram();
+
+          if (!tsProgram) {
+            return false;
+          }
+
+          const xstateProject = getOrCreateXStateProject(
+            await getTypeScriptModule(textDocument.uri),
+            tsProgram,
           );
 
-          connection.console.log(
-            `Hello in "${languageService
-              .getProgram()
-              ?.getCurrentDirectory()}", ${name}! You are viewing:\n\n${
-              languageService
-                .getProgram()!
-                .getSourceFile(env.uriToFileName(textDocument.uri))!.text
-            }`,
+          const machines = xstateProject.extractMachines(
+            env.uriToFileName(textDocument.uri),
           );
+
+          connection.console.log(JSON.stringify(machines));
+
           return true;
         });
+
+        async function getTypeScriptModule(uri: string) {
+          return (await projects.getProject(uri))
+            .getLanguageService()
+            .context.inject<Provide, 'typescript/typescript'>(
+              'typescript/typescript',
+            );
+        }
 
         async function getTypeScriptLanguageService(uri: string) {
           return (await projects.getProject(uri))
@@ -66,6 +84,19 @@ export const createXStateServerPlugin =
             .context.inject<Provide, 'typescript/languageService'>(
               'typescript/languageService',
             );
+        }
+
+        function getOrCreateXStateProject(
+          ts: typeof import('typescript'),
+          tsProgram: Program,
+        ) {
+          const existing = projectCache.get(tsProgram);
+          if (existing) {
+            return existing;
+          }
+          const xstateProject = createProject(ts, tsProgram);
+          projectCache.set(tsProgram, xstateProject);
+          return xstateProject;
         }
       },
     };
