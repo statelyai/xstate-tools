4b4e409709b45c5b20a7083b4000c448fc59b300
Farzad Yousefzadeh <farskid@gmail.com>
2024-01-12 13:58:12 +0200

    Add test for multiple machines in single file

--- new-packages/ts-project/__tests__/createMachine.test.ts
+++ new-packages/ts-project/__tests__/createMachine.test.ts
@@ -66,3 +66,71 @@ test('should extract createMachine with empty config', async () => {
     ]
   `);
 });
+
+test('should extract multiple createMachines in a single file', async () => {
+  const tmpPath = await testdir({
+    'tsconfig.json': JSON.stringify({}),
+    'index.ts': ts`
+      import { createMachine } from "xstate";
+
+      createMachine({
+        id: "machine1",
+      });
+
+      createMachine({
+        id: "machine2",
+      });
+    `,
+  });
+
+  const project = await createTestProject(tmpPath);
+
+  expect(project.extractMachines('index.ts')).toMatchInlineSnapshot(`
+    [
+      [
+        {
+          "edges": [],
+          "rootState": {
+            "data": {
+              "description": undefined,
+              "entry": [],
+              "exit": [],
+              "history": undefined,
+              "initial": undefined,
+              "invoke": [],
+              "metaEntries": [],
+              "tags": [],
+              "type": undefined,
+            },
+            "id": "",
+            "states": [],
+          },
+          "states": [],
+        },
+        [],
+      ],
+      [
+        {
+          "edges": [],
+          "rootState": {
+            "data": {
+              "description": undefined,
+              "entry": [],
+              "exit": [],
+              "history": undefined,
+              "initial": undefined,
+              "invoke": [],
+              "metaEntries": [],
+              "tags": [],
+              "type": undefined,
+            },
+            "id": "",
+            "states": [],
+          },
+          "states": [],
+        },
+        [],
+      ],
+    ]
+  `);
+});
